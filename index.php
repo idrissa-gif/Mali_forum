385ac7be5bce04282bca8f873492c97107e16023
idrissa-gif <idrissa@iut-dhaka.edu>
2022-04-30 16:29:33 +0600

    starting implementing index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title color>Bienvenue Au Mali Forum</title>
+</head>
+<body>
+    <header>
+        <title>
+            Bienvenue Au Maliba Forum
+        </title>        
+    </header>
+    <p> the world is just so you have to gc_collect_cycles gc_collect_cycles ob_clean </p>
+</body>
+</html>
