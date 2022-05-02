327009075f188937ae53224fad15b641491b5060
idrissa-gif <idrissa@iut-dhaka.edu>
2022-05-02 12:37:13 +0600

    advice.css

---
+++ advice.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel = "stylesheet" href = "style.css" ></link>
+</head>
+<body>
+    <arctile>
+        <header class = "header">
+            <a href = "https://www.google.com/" class = "logolink" ><img src = "https://images.unsplash.com/photo-1651316859299-e42c26e15cb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60" alt = "logo used from unsplash"/></a>
+            <h1 class = "heading--primary" > This should reprensent our logo </h1>
+        </header>
+        <section class = "introContainer">
+            <p class = "introContainer--text"> The steps we need to take to become professional web developer.</p>
+            <figure class = "webDev">
+                <img id = "Photo2" class = "webDev--img" src= "https://images.unsplash.com/flagged/photo-1550867571-6d0bc79eb6f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWxtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt = "Thank you Christina"\>
+                <figcaption class = "webDev--caption" >Pictures are taken free from unsplash</figcaption>
+            </figure>
+        </section>
+        <p class = "skills"> In order to become web developer ou need to have the following skills</p>
+        <details class = "skills-container">
+            <summary class = "heading--secondary"> Skills </summary>
+            <ul>
+                <li> 1.HTML/CSS.</li>
+                <li>2.JAVASCRIPT/JQUERY</li>
+                <li>3.CSS AND JAVASCRIPT FRAMEWORKS</li>
+                <li>4.CSS PREPROCESSING</li>
+                <li>5.VERSION CONTROL/GIT</li>
+                <li>6.RESPONSIVE DESIGN</li>
+                <li>7.TESTING/DEBUGGING</li>
+                <li>8.BROWSER DEVELOPER TOOLS</li>
+                <li>9.BUILDING AND AUTOMATION TOOLS/WEB PERFORMANCE</li>
+                <li>10.COMMAND LINE</li>
+            </ul>
+        </details>
+        <section class = "web-steps">
+            <h3 class = "heading--tertiary">Steps</h3>
+            <ol class = "web-steps--wrapper">
+                <li>CHOOSE A DEVELOPMENT SPECIALIZATION </li>
+                <li>GET AN EDUCATION:No specific degree or educational path exist for careers in web development, and it's possible to become a web developer with or without a formal education </li>
+                <li>DEVELOP TECHNICAL SKILLS: despite the real advantages that come with an organized eduction, web development is still a field in which concrete skills can trump education when it comes to employment opportunitiies.</li>
+                <li>PRACTICE YOUR CODING SKILLS: Web developers learn by doing. Set up a Github account and push code to the site.Excerpt FromWeb Design with HTML5 and CSS: Learn how to design, create and built responsive websites using the best HTML5 and CSS practices Sekuloski, Rick This material may be protected by copyright</li>
+                <li>LAUNCH AN ONLINE PORTFOLIO: Employers want to see results such as current examples of successful site and application builds. </li>
+                <li>WEB DEVELOPMENT CERTIFICATES: he certificate option is designed for targeted educational training and is intended for students who need practical and fundamental knowledge relating to web development.</li>
+            </ol>
+        </section>
+        <footer>
+            Made for the book , <a href = "">'Build responive web sites with HTML5 are CSS'</a> by <address> <a href = "https://theodorecoding.com/profile/l-rick-sekuloski">Rick Sekuloski</a></address>
+        </footer>
+    </arctile>
+</body>
+</html>
