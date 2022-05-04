c93238dd03843cfda2a37f065b6b620266b4b7d2
idrissa-gif <idrissa@iut-dhaka.edu>
2022-05-04 19:12:55 +0600

    commit

--- index.php
+++ index.php
@@ -2,15 +2,48 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A basic HTML5 video link">
+    <meta name = "viewport" content ="width = device-width">
     <title>Embed Video in HTML5</title>
     <link rel = "stylesheet" href = "css/style.css">
+    <style>
+        body{
+            background-color: black;
+        }
+        @media  screen and (min-width: 320) {
+            body
+            {
+                background-color:blue;
+            }
+        }
+        @media  screen and (min-width: 550) {
+            body
+            {
+                background-color:green;
+            }
+        }
+        @media  screen and (min-width: 768) {
+            body
+            {
+                background-color:yellow;
+            }
+        @media  screen and (min-width: 960) {
+            body
+            {
+                background-color:orangered;
+            }
+        }
+        
+    </style>
+
 </head>
 <body>
-    <video src= "video/pexels.mp4" controls preload = "auto" loop poster = "video/monkey.jpeg" >
+    <video controls preload = "auto" loop >
         There must be some kind of problem loading this vid, are you using updated browser!
         <source src= "video/pexels.mp4" type="video/mp4">
-  </video>
+    </video> 
+    <div>
+    <iframe src='https://www.youtube.com/embed/KQ21gTWU-rM' frameborder='0' allowfullscreen></iframe>
+    </div>
 </body>
 </html>
